c665f6910db8eed439bf5aee9cc4169e6f770fd8
wilsonaganyo@gmail.com <wilsonaganyo@gmail.com>
2024-02-28 11:46:47 +0300

    created view for the new_topic view

--- learning_journal/ljApp/views.py
+++ learning_journal/ljApp/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import TopicForm
 from . models import Topic
 
 # Create your views here.
@@ -17,3 +18,9 @@ def topic(request, topic_id):
     entries = topic.entry_set.order_by('date_added')
     context = {'topic':topic, 'entries':entries}
     return render(request, 'ljApp/topic.html', context)
+
+def new_topic(req):
+    if req.method !='POST':
+        #no data submitted, create a blank form
+
+    return render(req, 'ljApp/new_topic.html')
