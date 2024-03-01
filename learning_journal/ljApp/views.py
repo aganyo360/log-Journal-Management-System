9c6d7f9f9379f8cd763ca3be2adf322ea4607475
wilsonaganyo@gmail.com <wilsonaganyo@gmail.com>
2024-03-01 09:44:09 +0300

    modified entry html templated

--- learning_journal/ljApp/views.py
+++ learning_journal/ljApp/views.py
@@ -49,7 +49,12 @@ def new_entry(request, topic_id):
     context ={'topic':topic, 'form':form}
     return render(request, 'ljApp/new_entry.html', context)
         
-def edit_entry(req):
-    return render(req, 'ljApp/edit_entry.html')
+def edit_entry(request, entry_id):
+    entry = Entry.objects.get(id=entry_id)
+    topic = entry.topic
+
+    if request.method != 'POST':
+        
+    return render(request, 'ljApp/edit_entry.html')
 
 
