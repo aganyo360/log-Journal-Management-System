4529c3a534fa203a0a35bb2c046bfd4237663f2d
aganyo360 <wilsonaganyo@gmail.com>
2024-03-10 07:33:31 +0300

    functions refresher

--- Functions/variable_length_argument.py
+++ Functions/variable_length_argument.py
@@ -3,5 +3,17 @@
 # the asterisk is used to pass multiple values
 
 def v_l_a(*var):
+    print(var)
     return sum(var)
 print(v_l_a(10,20,20))
+
+
+#Swapping the numbers with temp variable
+def Swaping_Numbers(x,y):
+    print(x,y)
+    temp=x
+    x=y
+    y=temp
+    
+    print(x,y)
+Swaping_Numbers(10,20)
