7b7a0aaa53b421c529cfc2f5c704862f9f76d7c4
aganyo360 <wilsonaganyo@gmail.com>
2024-03-10 07:42:16 +0300

    functions refresher

---
+++ Functions/anonymous_fun.py
@@ -0,0 +1,7 @@
+# anonymous function is a function without a name
+# instead of the def keyword it uses lambda as the keyword to define the anonymous fun
+
+# syntax:  var= lambda arguments: expression
+
+cube = lambda y: y**3
+print(cube(2))
